af96404be1038be399310deb07f51b6ab6b8af0a
Vineeth T R <Vineeth.TR@gds.ey.com>
2021-07-04 19:45:20 +0530

    App init

---
+++ sw.js
@@ -0,0 +1,19 @@
+
+// Install service worker
+self.addEventListener('install' , e => {
+    e.waitUntil(
+        caches.open('static').then(cache => {
+            return cache.addAll([
+                './' , 
+                './images/android-chrome-192x192.png' 
+            ])
+        })
+    )
+});
+
+// fetch
+self.addEventListener('fetch', e => {
+    console.log('Intercepting fetch request ', e.request.url)
+})
+
+
